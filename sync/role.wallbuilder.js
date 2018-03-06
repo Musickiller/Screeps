efebebaabc3bc50d94cee9c81558f0c27a309e98
Musickiller <musickiller@yandex.ru>
2018-03-06 11:19:11 +0300

    make wall maker from fixer

--- sync/role.wallbuilder.js
+++ sync/role.wallbuilder.js
@@ -5,28 +5,30 @@ var roleFixer = {
     run: function(creep) {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
-            	creep.memory.building = false;
-            	creep.say('HARVEST');
+		    creep.memory.building = false;
+		    creep.say('HARVEST');
 	    } else if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say('FIX');
+		    creep.memory.building = true;
+		    creep.say('FIX');
 	    } else if (creep.memory.building) {
-	        var structs = creep.room.find(FIND_MY_STRUCTURES);
-          var targets = new Array
-          for (struct in structs) {
-              if ( struct.hitsMax/struct.hits < 0.5 && struct.structureType != STRUCTURE_WALL ) {
-                  targets.push(struct)
-              }
-          }
-          if(targets.length) {
-              if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-              }
-          }
+		    var walls = _.filter(creep.room.find(FIND_MY_STRUCTURES), (structure) => structure.structureType == 'STRUCTURE_WALL');
+		    var minHits = 90000
+		    var target
+		    for (var wall in walls) {
+			    if ( wall.hits < minHits ) {
+				    target = wall
+				    minHits = wall.hits
+			    }
+		    }
+		    if(target) {
+			    if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+				    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+			    }
+		    }
 	    } else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            	if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                	creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+		    var sources = creep.room.find(FIND_SOURCES);
+		    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+			    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             	}
 	    }
 	}
