efe8b074ae32dc67d22e85566349999fe29cb829
Musickiller <musickiller@yandex.ru>
2018-03-06 10:07:04 +0300

    more help

--- sync/console_commands.js
+++ sync/console_commands.js
@@ -4,4 +4,6 @@ Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Builder' + Game.time, {memo
 // room help:
 for (h in Game.rooms['E41N49']) { console.log(h) }
 // fing in rooms:
-Game.rooms['E41N49'].find(FIND_CONSTRUCTION_SITES);
+Game.rooms['E41N49'].find(FIND_CONSTRUCTION_SITES)
+//help on construction sites
+for (prpt in Game.rooms['E41N49'].find(FIND_CONSTRUCTION_SITES)[0]) { console.log(prpt) }
