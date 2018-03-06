27c0813e8bf139c43b936fe3ac73cde93caa965e
Musickiller <musickiller@yandex.ru>
2018-03-06 11:20:22 +0300

    sort, more comms

--- sync/console_commands.js
+++ sync/console_commands.js
@@ -1,9 +1,16 @@
-Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Builder' + Game.time, {memory: {role: 'builder'}}); 
+// CREEPS
+// list creeps
+for (var crp in Game.creeps) { console.log(crp) }
+// spawn
+Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Builder' + Game.time, {memory: {role: 'builder'}})
 
 // ROOMS
 // room help:
 for (h in Game.rooms['E41N49']) { console.log(h) }
 // fing in rooms:
 Game.rooms['E41N49'].find(FIND_CONSTRUCTION_SITES)
-//help on construction sites
+
+
+// STRUCTURES
+// help on construction sites
 for (prpt in Game.rooms['E41N49'].find(FIND_CONSTRUCTION_SITES)[0]) { console.log(prpt) }
